28d6a7bced4a06313cb26a498e07dfc3682ccfd3
Joey Chen <jay.hatelove@gmail.com>
2019-07-30 15:47:03 +0800

    add test: init builder with specific date, then change its month

--- JavaLikeDateTime.Tests/DateTimeBuilderTests.cs
+++ JavaLikeDateTime.Tests/DateTimeBuilderTests.cs
@@ -9,9 +9,11 @@ namespace JavaLikeDateTime.Tests
     public class DateTimeBuilderTests
     {
         private const int SpecificYear = 2019;
+        private const int SpecificMonth = 7;
+        private const int SpecificDay = 10;
         private readonly DateTimeBuilder _dateTimeBuilder;
-        private readonly ITestOutputHelper _testOutputHelper;
         private readonly DateTime _specificDate = new DateTime(2020, 9, 1);
+        private readonly ITestOutputHelper _testOutputHelper;
 
         public DateTimeBuilderTests(ITestOutputHelper testOutputHelper)
         {
@@ -36,15 +38,15 @@ namespace JavaLikeDateTime.Tests
         [Fact]
         public void build_with_specific_month()
         {
-            var actual = _dateTimeBuilder.AtYear(SpecificYear).AtMonth(7).Build();
-            ResultShouldBe(actual, new DateTime(SpecificYear, 7, 1));
+            var actual = _dateTimeBuilder.AtYear(SpecificYear).AtMonth(SpecificMonth).Build();
+            ResultShouldBe(actual, new DateTime(SpecificYear, SpecificMonth, 1));
         }
 
         [Fact]
         public void build_with_specific_day()
         {
-            var actual = _dateTimeBuilder.AtYear(SpecificYear).AtMonth(7).AtDay(10).Build();
-            ResultShouldBe(actual, new DateTime(SpecificYear, 7, 10));
+            var actual = _dateTimeBuilder.AtYear(SpecificYear).AtMonth(SpecificMonth).AtDay(SpecificDay).Build();
+            ResultShouldBe(actual, new DateTime(SpecificYear, SpecificMonth, SpecificDay));
         }
 
         [Fact]
@@ -52,6 +54,13 @@ namespace JavaLikeDateTime.Tests
         {
             var actual = DateTimeBuilder.InitBuilder(_specificDate).Build();
             ResultShouldBe(actual, _specificDate);
+        }
+
+        [Fact]
+        public void init_with_specific_date_and_change_month()
+        {
+            var actual = DateTimeBuilder.InitBuilder(_specificDate).AtMonth(SpecificMonth).Build();
+            ResultShouldBe(actual, new DateTime(_specificDate.Year, SpecificMonth, _specificDate.Day));
         }
 
         private static void ResultShouldBe(DateTime actual, DateTime expected)
